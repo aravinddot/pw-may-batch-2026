4f0567f5b734b38ecf67db162202cd93608df3aa
Aravind112703 <aravindmathi93@gmail.com>
2026-05-21 19:05:24 +0530

    conditional statement

---
+++ conditionalStatement.js
@@ -0,0 +1,56 @@
+// if - else
+
+// switch case
+
+// const value = false
+
+// if(value) {
+//     console.log("true executed");
+// } else {
+//     console.log("false executed");
+
+// }
+
+// const marks = 49;
+
+// if (marks >= 90) {
+
+//   console.log("Grade A");
+
+// } else if (marks >= 75) {
+
+//   console.log("Grade B");
+
+// } else if (marks >= 50) {
+
+//   console.log("Grade C");
+
+// } else {
+
+//   console.log("fail");
+
+// }
+
+
+
+
+// switch case
+
+
+const day = 1
+
+switch(day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2: 
+        console.log("Tuesday");
+        break;
+    default:
+        console.log("invalid day");
+        
+}
+
+
+
+
--- operators.js
+++ operators.js
@@ -19,77 +19,167 @@
 
 // addition
 
-console.log(a + b); // addition
+// console.log(a + b); // addition
 
-console.log(a - b); // subraction
+// console.log(a - b); // subraction
 
-console.log(a * b); // multiplication
+// console.log(a * b); // multiplication
 
-console.log(a / b); // division
+// console.log(a / b); // division
 
-console.log(a % b); // modules
+// console.log(a % b); // modules
 
-console.log(a ** b); //power or exponentation. 10 x 10 x 10
-
-
-
-// Assignment operators
-
- let c = 15 // =
-
- c += 5 // addition
-
- c -= 5 // subraction
-
-c *= 5 // multiplication
-
-c/= 5  //division
-
-console.log(c);
+// console.log(a ** b); //power or exponentation. 10 x 10 x 10
 
 
-// comparsion operator
+
+// // Assignment operators
+
+//  let c = 15 // =
+
+//  c += 5 // addition
+
+//  c -= 5 // subraction
+
+// c *= 5 // multiplication
+
+// c/= 5  //division
+
+// console.log(c);
 
 
-// == - equal to - compare only values
+// // comparsion operator
 
-const valueOne = 12
-const valueTwo = 12
 
-console.log(valueOne == valueTwo);
+// // == - equal to - compare only values
 
-// === - strict equal to - compare values and data type
+// const valueOne = 12
+// const valueTwo = 12
 
-console.log(valueOne === valueTwo);
+// console.log(valueOne == valueTwo);
 
-// != - not equal - comapare only values
+// // === - strict equal to - compare values and data type
 
-console.log(valueOne != valueTwo);
+// console.log(valueOne === valueTwo);
 
-// !== - strictly not equal to - compare values and data types
+// // != - not equal - comapare only values
 
-console.log(valueOne !== valueTwo);
+// console.log(valueOne != valueTwo);
 
-const a = 10
-const b = 10
+// // !== - strictly not equal to - compare values and data types
 
-// greater than
+// console.log(valueOne !== valueTwo);
 
-console.log(a > b);
+// const a = 10
+// const b = 10
 
-// less than
+// // greater than
 
-console.log(b < a);
+// console.log(a > b);
 
-// greater than or equal to
+// // less than
 
-console.log(a >= b);
+// console.log(b < a);
 
-// less than or equal to
+// // greater than or equal to
 
-console.log(b <= a);
+// console.log(a >= b);
 
-console.log();
+// // less than or equal to
+
+// console.log(b <= a);
+
+// console.log();
+
+
+
+// logical operator - 
+
+
+// And
+// OR
+// NOT
+
+
+// const a = false
+// const b = false
+
+// console.log(a && b);
+
+
+// console.log(a || b);
+
+
+// console.log(!a);
+
+
+
+// increment & decrement operator
+
+
+// pre & post 
+
+//post
+
+let c = 10
+
+//c++ // 10 + 1
+
+// console.log(c++);
+
+// console.log(c);
+
+
+
+
+// let d = 10
+
+// console.log(++d);
+
+
+// console.log(--c);
+
+
+
+// String operator
+
+
+// const automationTool = "playwright"
+// const company = "Microsoft"
+
+
+// console.log(automationTool + company + "Automation Tool");
+
+// console.log(`${automationTool} ${company} Automation Tool`);
+
+
+
+// type operator
+
+// const a = 10
+// console.log(typeof a);
+// console.log(typeof "playwright");
+
+
+// ternary operator
+
+const age = 20;
+
+const result = age >= 18 ? "Adult" : "Minor"
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
